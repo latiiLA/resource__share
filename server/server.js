3f670650704ef8de92fa9c36bebf7f3fc6b0d0cf
Lata <amelatbay@gmail.com>
2024-05-08 15:42:07 +0300

    Database communications, routes, models and others are implemented

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
+app.use(cors());
+app.use(express.json()); // Add this line to parse JSON requests
 
+// const User = require("./Models/userModel");
+// const { createUser } = require("./Controller/userController");
+const authRoute = require("./Route/authRoute");
 
-app.get('/login', (req, res)=>{
-    res.send({message: "message already sent"});
-})
+mongoose
+  .connect(process.env.URL)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Mongodb connected and Listening on the port ${process.env.PORT}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(process.env.PORT || 2000 , ()=>{
-    console.log(`Listening on the port ${process.env.PORT}`)
-});
+app.use("/auth", authRoute);
