d4e3ea3b1a57f6fbb9951b2f828fd497790493e6
Lata <amelatbay@gmail.com>
2024-05-08 15:40:40 +0300

    configurations and routes are added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+// imported libraries
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import { Box } from "@mui/material";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
-    <div className="App">
-      <div>Home created</div>
-    </div>
+    <Box>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </Box>
   );
 }
 
