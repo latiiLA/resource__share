0e54d9bd7d5b6e029c9d8bff44b119b7d98a61a5
Lata <amelatbay@gmail.com>
2024-05-14 13:59:24 +0300

    routes are modified

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import { Box } from "@mui/material";
 import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/signup" element={<SignUp />} />
+          <Route  path="/signup" element={<SignUp />} />
+          <Route  path="/login" element={<Login />} />
         </Routes>
       </Router>
     </Box>
